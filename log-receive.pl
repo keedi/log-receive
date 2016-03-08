8f2f6725ea6e7fa257e8a264e0da420d502df870
Keedi Kim <keedi.k@gmail.com>
2016-03-08 21:17:09 +0900

    Import materializecss and use it for '/' address

--- log-receive.pl
+++ log-receive.pl
@@ -21,6 +21,55 @@ my $LOG_DIR = app->config->{log_dir};
 app->log->info("log dir: $LOG_DIR");
 path($LOG_DIR)->mkpath;
 
+plugin "AssetPack" => { source_paths => [qw( assets )] };
+
+app->asset(
+    "app.css" => qw(
+        sass/main.scss
+        ),
+);
+
+app->asset(
+    "app.js" => qw(
+        http://code.jquery.com/jquery-2.2.0.min.js
+
+        /vendor/Materialize/js/initial.js
+        /vendor/Materialize/js/jquery.easing.1.3.js
+        /vendor/Materialize/js/animation.js
+        /vendor/Materialize/js/velocity.min.js
+        /vendor/Materialize/js/hammer.min.js
+        /vendor/Materialize/js/jquery.hammer.js
+        /vendor/Materialize/js/global.js
+        /vendor/Materialize/js/collapsible.js
+        /vendor/Materialize/js/dropdown.js
+        /vendor/Materialize/js/leanModal.js
+        /vendor/Materialize/js/materialbox.js
+        /vendor/Materialize/js/parallax.js
+        /vendor/Materialize/js/tabs.js
+        /vendor/Materialize/js/tooltip.js
+        /vendor/Materialize/js/waves.js
+        /vendor/Materialize/js/toasts.js
+        /vendor/Materialize/js/sideNav.js
+        /vendor/Materialize/js/scrollspy.js
+        /vendor/Materialize/js/forms.js
+        /vendor/Materialize/js/slider.js
+        /vendor/Materialize/js/cards.js
+        /vendor/Materialize/js/chips.js
+        /vendor/Materialize/js/pushpin.js
+        /vendor/Materialize/js/buttons.js
+        /vendor/Materialize/js/transitions.js
+        /vendor/Materialize/js/scrollFire.js
+        /vendor/Materialize/js/date_picker/picker.js
+        /vendor/Materialize/js/date_picker/picker.date.js
+        /vendor/Materialize/js/character_counter.js
+        /vendor/Materialize/js/carousel.js
+
+        /coffee/main.coffee
+        ),
+);
+
+get "/" => "index";
+
 post "/id" => [ format => ["json"] ] => sub {
     my $c = shift;
 
